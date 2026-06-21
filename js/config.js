4d7f2875832acd9380a19aedd8eb4d29f3c14cbf
Joseph | Gestión Fitness <josephantoninos@gmail.com>
2026-06-21 02:55:48 -0600

    Actualizar config.js
    actualización de ejercicios

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@ const ACTIVITIES = [
 'Fuerza A', 'Circuitos', 'Cardio', 'Fuerza B', 'Zumba', 'Descanso', 'Descanso'
 ];
 const exercisesA = ['Sentadilla Goblet 3x10', 'Press Banco Mancuernas 3x10', 'Remo 3x10', 'Plancha 3x30 seg'];
-const exercisesB = ['Peso Muerto Rumano 3x10', 'Press Militar 3x10', 'Jalón al Pecho 3x10', 'Pallof Press 3x12'];
-const videoIdsA = ['F3-DxxLNrqw', '48L0oQApm_0', '2z3tI5c7Oc8', '3AM7L2k7BEw'];
+const exercisesB = ['Leg Curl 3x10', 'Press Militar 3x10', 'Jalón al Pecho 3x10', 'Pallof Press 3x12'];
+const videoIdsA = ['42Io57WaiEI', '48L0oQApm_0', '2z3tI5c7Oc8', '3AM7L2k7BEw'];
 const videoIdsB = ['SAdzug-v7D0', 'IuR427toLXE', 'udUQ99c3oRE', 'PSb76pXrGNI'];
